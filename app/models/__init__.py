9f2a94c1f95c1d9fef9d27ec4801cd79451f97ed
zk222ac001 <zk222ac@gmail.com>
2026-09-13 12:58:31 +0200

    phase2: register resume models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.application import Application
 from app.models.candidate import CandidateProfile
 from app.models.job import JobPosting
+from app.models.resume import JobMatch, Resume
 
-__all__ = ["Application", "CandidateProfile", "JobPosting"]
+__all__ = ["Application", "CandidateProfile", "JobMatch", "JobPosting", "Resume"]
